4a20824a465a1eb2352fb890897cf6f359d6bdf3
GustavSkou <Gustav.skou.rasmussen@gmail.com>
2024-09-22 23:55:10 +0200

    potes

---
+++ potens/Program.cs
@@ -0,0 +1,10 @@
+﻿long pow(int a, int n) {
+      if (n == 0){
+            return 1;
+      }
+      return a * pow(a, n-1);
+}
+
+Console.WriteLine(pow(2, 2));
+Console.WriteLine(pow(10, 10));
+Console.WriteLine(pow(100, 0));
