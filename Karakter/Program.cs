aadccd14441eedc0baf4bf27ef75211a5864e53c
GustavSkou <Gustav.skou.rasmussen@gmail.com>
2024-09-25 13:32:40 +0200

    Karakter (try, throw, catch)

---
+++ Karakter/Program.cs
@@ -0,0 +1,45 @@
+﻿public class DumpetException : Exception 
+{
+    public DumpetException(string message) : base(message)
+    {}
+
+}
+
+class AvgGradeError{
+
+    public int[] grades;
+    
+    public AvgGradeError(int[] someGrades) {
+        grades = someGrades;
+    }
+
+    public int GetGrade(int courseId) {
+        
+        int grade = grades[courseId];
+        
+        if (grade < 02) {
+            throw new DumpetException($"Index {courseId} er dumpet");
+        }
+    
+        return grade;
+    }
+}
+
+class Program {
+    static void Main () {
+        int[] grades = {4, 7, 02, 00, 10, 4, 12};
+        double sum = 0, count = 0;
+        AvgGradeError algotihm = new AvgGradeError(grades);
+
+        for (int i = 0; i < grades.Length; i++) {
+            try {
+                sum = sum + algotihm.GetGrade(i);
+                count++;
+            }
+            catch (DumpetException e) {
+                Console.WriteLine(e.Message);
+            }   
+        }
+        Console.WriteLine(sum/count);
+    }
+}
